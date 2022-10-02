565cc0ad211de569f89b5bb31d3be432a5f4ceea
Aman Kumar <70056787+amankumr@users.noreply.github.com>
2022-10-02 15:11:54 +0530

    Update main.py
    Now The Code produces More Stronger PAssword

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import random
 import string
 print("Welcome to Password Picker")
 
-adjectives = ['sleepy', 'slow', 'smelly', 'wet', 
-              'fat', 'red', 'orange', 'yellow', 'green', 
-              'blue', 'purple', 'fluffy', 'white', 'proud', 'brave']
+adjectives = ['slEEy', 'sLoW', 'sMELly', 'wEt', 
+              'fAt', 'rEd', 'OraNGe', 'yeLLow', 'gRee7N', 
+              'bLUe', 'pURpLe', 'FLuffy', 'WhiTe', 'p8ROUd', 'BRavE']
 
-nouns = ['apple', 'dinosaur', 'ball', 'toaster', 
-         'goat', 'dragon', 'hammer', 'duck', 'panda']
+nouns = ['aPp*Le', 'DiN#oSAur', 'BaLl', 'tOAs+tEr', 
+         'Go(At', 'Dra$GoN', 'h/aMMer', 'Du=Ck', 'pa+NdA']
          
 adjective = random.choice(adjectives)
 noun = random.choice(nouns)
